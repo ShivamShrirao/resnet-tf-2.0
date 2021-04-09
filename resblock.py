4eae1c2f4b4940be6cf8b02bfcaf32cf0a37f6d4
ShivamShrirao <shivamshrirao@gmail.com>
2021-04-10 02:10:59 +0530

    debugs

--- resblock.py
+++ resblock.py
@@ -3,7 +3,7 @@ import tensorflow.keras.layers as layers
 
 
 def norm_act(x, activation=tf.nn.relu):
-    x = layers.BatchNormalization(axis=1)(x)
+    x = layers.BatchNormalization()(x)
 
     if activation is not None:
         if activation == 'leaky_relu':
@@ -32,12 +32,12 @@ def BasicBlock(inp, filters, strides=1, activation=tf.nn.relu, dp_rate=0,
 
     x = norm_act(inp, activation=activation)
 
-    if in_filters != filters:   # use conv_shortcut to increase the filters of residual.
-        residual = conv_norm(x, filters, strides=1, activation=activation, do_norm_act=False)
+    if in_filters != filters:   # use conv_shortcut to increase the filters of identity.
+        identity = conv_norm(x, filters, strides=1, activation=activation, do_norm_act=False)
     elif strides > 1:               # else just downsample or conv1x1 with strides can be tried.
-        residual = layers.MaxPool2D()(inp)
+        identity = layers.MaxPool2D()(inp)
     else:                           # or keep the same.
-        residual = inp
+        identity = inp
 
     x = conv_norm(x, filters, kernel_size=3, activation=activation, strides=strides)
     x = conv_norm(x, filters, kernel_size=3, activation=activation, do_norm_act=False)
@@ -45,7 +45,7 @@ def BasicBlock(inp, filters, strides=1, activation=tf.nn.relu, dp_rate=0,
     if dp_rate:
         x = layers.Dropout(dp_rate)(x)
 
-    x = layers.Add()([residual, x])
+    x = layers.Add()([identity, x])
 
     if make_model:                  #  (just ignore)
         m = tf.keras.Model(inputs=inp, outputs=x, name=f"BasicBlock_{suffix}")
@@ -66,12 +66,14 @@ def Bottleneck(inp, filters, strides=1, activation=tf.nn.relu, expansion=4,
 
     x = norm_act(inp, activation=activation)
 
-    if in_filters != out_filters:   # use conv_shortcut to increase the filters of residual.
-        residual = conv_norm(x, out_filters, strides=1, activation=activation, do_norm_act=False)
-    elif strides > 1:               # else just downsample or conv1x1 with strides can be tried.
-        residual = layers.MaxPool2D()(inp)
-    else:                           # or keep the same.
-        residual = inp
+    identity = inp
+    conv_shortcut = False
+    if in_filters != out_filters:   # use conv_shortcut to increase the filters of identity.
+        identity = conv_norm(x, out_filters, strides=1, activation=activation, do_norm_act=False)
+        conv_shortcut = True
+    if strides > 1:
+        mip = identity if conv_shortcut else inp
+        identity = layers.MaxPool2D()(mip)
 
     x = conv_norm(x, filters, kernel_size=1, activation=activation)      # contract
     x = conv_norm(x, filters, kernel_size=3, activation=activation, strides=strides)
@@ -80,7 +82,7 @@ def Bottleneck(inp, filters, strides=1, activation=tf.nn.relu, expansion=4,
     if dp_rate:
         x = layers.Dropout(dp_rate)(x)
 
-    x = layers.Add()([residual, x])
+    x = layers.Add()([identity, x])
 
     if make_model:                  #  (just ignore)
         m = tf.keras.Model(inputs=inp, outputs=x, name=f"Bottleneck_{suffix}")
--- resnet.py
+++ resnet.py
@@ -8,11 +8,13 @@ from resblock import norm_act, conv_norm, BasicBlock, Bottleneck
 
 class Resnet():
     def __init__(self, block, filters_per_stack=[64, 128, 256, 512], num_repeats=[3,4,6,3], strides=[1,2,2,2],
-                 dp_rate=0, activation=tf.nn.relu, inputs=None, input_shape=(224, 224, 3), num_classes=1024):
+                 expansion=4, dp_rate=0, activation=tf.nn.relu, inputs=None, input_shape=(224, 224, 3),
+                 num_classes=1024):
         self.block = block
         self.dp_rate = dp_rate
         self.activation = activation
         self.num_classes = num_classes
+        self.expansion = expansion
         self.input_shape = input_shape
         if inputs is None:
             self.inputs = layers.Input(shape=input_shape)
@@ -23,15 +25,18 @@ class Resnet():
         self.filters_per_stack = filters_per_stack
         self.num_repeats = num_repeats
         self.strides = strides
-        self.build_model()
+        o = self.build_model()
+        model = tf.keras.Model(inputs=self.inputs, outputs=o)
 
     def build_model(self, include_top=True):
         x = self.inputs
+        # stem
         x = conv_norm(x, 64, kernel_size=7, strides=2, activation=self.activation, do_norm_act=False)
         x = layers.MaxPooling2D(3, strides=2)(x)
 
-        for i in range(len(filters_per_stack)):
-            x = stack(x, self.block, self.filters_per_stack[i], self.strides[i], repeat=self.num_repeats[i],
+        # body
+        for i in range(len(self.filters_per_stack)):
+            x = self.stack(x, self.block, self.filters_per_stack[i], self.strides[i], repeat=self.num_repeats[i],
                       dp_rate=self.dp_rate)
 
         x = norm_act(x)
@@ -41,6 +46,7 @@ class Resnet():
             x = layers.GlobalAveragePooling2D()(x)
             x = layers.Dense(self.num_classes)(x)
             x = layers.Softmax(axis=-1)(x)
+            return x
 
     def stack(self, x, block, filters, stride1=2, dp_rate=0, repeat=3, suffix=1):
         i = 0
@@ -54,7 +60,11 @@ class Resnet():
         return x
 
     def preprocess_input(self):
-        pass
+        raise NotImplementedError
+
+
+def RESNEXT():
+    raise NotImplementedError
 
 
 def Resnet18(inputs=None,
